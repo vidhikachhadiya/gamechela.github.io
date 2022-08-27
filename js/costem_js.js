75a624f18bfe66ff463d268236dca75c2ff175e7
vidhikachhadiya <99782233+vidhikachhadiya@users.noreply.github.com>
2022-08-27 16:06:54 +0530

    11222

---
+++ js/costem_js.js
@@ -0,0 +1,88 @@
+
+
+// -------------loader
+  $(window).load(function () {
+    $(".loader").fadeOut("slow")
+});
+
+// -------=========== back to top btn ----
+
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() - 200 > 0) {
+            $('#to-top').stop().slideDown('fast'); // show the button
+        } else {
+            $('#to-top').stop().slideUp('fast'); // hide the button
+        }
+    });
+});
+
+$(function () {
+    // previous detection logic
+
+    $("#to-top").on("click", function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 200);
+    });
+});
+
+// ====================  type writer ===========
+
+var TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 1000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #cfcfcf}";
+    document.body.appendChild(css);
+};
+
